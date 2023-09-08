7017d6e31f53584930c0ff66af7cce26333c7c30
Joe Mucchiello <jmucchiello@gmail.com>
2023-09-08 10:13:01 -0400

    bugged 1.0.2

--- README.md
+++ README.md
@@ -26,8 +26,21 @@ Weapons, buildings, and villagers are not consumed in these recipes.
 * 5x Wood, 1x Villager -> 5x Stick _in 45s_
 * 1x Lumber, 1x Axe -> 2x Stick _in 15s_
 
+### Magic Dust
+* 1x Gold Ore, 1x Sand, 1x Campfire, 1x Wizard/Mage -> 1x Magic Dust, 1x Wizards/Mage _in 60s_<br>(The campfire is destroyed. This recipe does not work on a stove.)
+
+## Rebalancing
+
+* The gold mine usually gives you gold ore, stone, or golg coin in equal porportion (33%/33%/33%). This tweak makes the distribution 60%/20%/20% in favor of gold ore.
+
 ## Future Ideas
 
 If you think there should be others, let me know here or with a request on Github.
 
 Github: https://github.com/jmucchiello/stacklands-resources
+
+## History
+
+1.0.2 magic dust recipe and gold mine tweak<br/>
+1.0.1 fixes to metal recipes<br/>
+1.0.0 initial release
--- ResourcesMod.cs
+++ ResourcesMod.cs
@@ -9,20 +9,37 @@ namespace ResourcesModNS
     {
         public static ResourcesMod instance;
 
-        public override void Ready()
-        {
-            instance = this;
-            Logger.Log("Ready!");
-        }
+        public static void Log(string msg) => instance.Logger.Log(msg);
+        public static void LogError(string msg) => instance.Logger.LogError(msg);
 
         public void Awake()
         {
+            instance = this;
             Harmony.PatchAll();
+            modifyGoldMine = new ConfigEntry<bool>("resourcesmod_config_goldmine", Config, true, new ConfigUI()
+            {
+                NameTerm = "resourcesmod_tweak_goldmine",
+                TooltipTerm = "resourcesmod_tweak_goldmin_tooltip",
+            });
         }
 
-        public void Log(string message)
+        private ConfigEntry<bool> modifyGoldMine;
+
+        public override void Ready()
         {
-            Logger.Log(message);
+            Config.OnSave += MinorBalanceTweaks;
+            WorldManager.instance.actionTimeBases.Add(new ActionTimeBase((ActionTimeParams p) =>
+                p.villager.Id == Cards.lumberjack && p.baseCard.CardsInStackMatchingPredicate(card => card.Id == Cards.wood || card.Id == Cards.stick).Any(), 0.5f));
+            Logger.Log("Ready!");
+        }
+
+        public void MinorBalanceTweaks()
+        {
+            CardData cardData = WorldManager.instance.GameDataLoader.GetCardFromId(Cards.gold_mine);
+            if (cardData is Harvestable h)
+            {
+                h.MyCardBag.Chances.FirstOrDefault(x => x.Id == Cards.gold_ore).Chance = modifyGoldMine.Value ? 3 : 1;
+            }
         }
     }
 }
